4cd292d26d43e4befd9604c17b580a9e71f340b7
Pármenas Haniel <parmenashp@gmail.com>
2023-02-28 17:52:32 -0300

    Use dataclass for custom emojis

--- bot/helpers/emojis.py
+++ bot/helpers/emojis.py
@@ -1,11 +1,15 @@
 import discord
+from dataclasses import dataclass
 
 
+class ConcatenatablePartialEmoji(discord.PartialEmoji):
+    """A partial emoji that can be concatenated with strings."""
+
+    def __add__(self, other):
+        return str(self) + str(other)
+
+
+@dataclass(frozen=True, init=False)
 class Emojis:
-    @staticmethod
-    def small_x_mark():
-        return discord.PartialEmoji(name="small_x_mark", id=1039023797396848730)
-
-    @staticmethod
-    def small_check_mark():
-        return discord.PartialEmoji(name="small_check_mark", id=1039025339436908544)
+    small_x_mark = ConcatenatablePartialEmoji(name="small_x_mark", id=1039023797396848730)
+    small_check_mark = ConcatenatablePartialEmoji(name="small_check_mark", id=1039025339436908544)
